2229297d981e96711fabc8821f9048e447d4d2ee
ERSUCC <isaacbrown5763@gmail.com>
2026-06-01 14:43:48 -0500

    Infrastructure: Execute auto-conversions concurrently

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,56 @@
 <script setup lang="ts">
   import { useTemplateRef } from "vue";
 
+  interface ConvertedFile {
+    name: string;
+    data: Blob;
+  }
+
   const input = useTemplateRef("input");
 
   function convert(): void {
-    for (const file of input.value?.files ?? new FileList()) {
+    Promise.allSettled(convertFiles(input.value?.files ?? new FileList())).then(results => {
+      if (results.some(result => result.status == "rejected")) {
+        window.alert("Not all models were successfully auto-converted. Downloading successful conversions.");
+      }
+
+      for (const result of results) {
+        if (result.status == "fulfilled") {
+          downloadBlob(result.value.name, result.value.data);
+        }
+      }
+    }, window.alert);
+  }
+
+  function* convertFiles(files: FileList): Generator<Promise<ConvertedFile>> {
+    for (const file of files) {
       const data = new FormData();
 
       data.append("model", file);
 
-      fetch("http://localhost:4242/convert", {
+      yield fetch("http://localhost:4242/convert", {
         method: "POST",
         body: data
-      }).catch(console.error).then(async response => {
-        const blob: Blob | undefined = await response?.blob()
-
-        if (blob) {
-          const link: HTMLAnchorElement = document.createElement("a");
-
-          link.href = URL.createObjectURL(blob);
-          link.download = `${file.name.substring(0, file.name.lastIndexOf("."))}.nlogox`;
-
-          link.click();
+      }).then(async response => {
+        if (response.status != 200) {
+          return Promise.reject("Failed to auto-convert model.");
         }
-      })
+
+        return {
+          name: file.name.replace(/.nlogo(3d)?$/, ".nlogox$1"),
+          data: await response.blob(),
+        };
+      }, Promise.reject);
     }
+  }
+
+  function downloadBlob(name: string, data: Blob): void {
+    const link: HTMLAnchorElement = document.createElement("a");
+
+    link.href = URL.createObjectURL(data);
+    link.download = name;
+
+    link.click();
   }
 </script>
 
