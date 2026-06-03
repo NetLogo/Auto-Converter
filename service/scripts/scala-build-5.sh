10dd2c286c205e703967a3e8bf2da4deb38e92c6
ERSUCC <isaacbrown5763@gmail.com>
2026-06-03 17:36:56 -0500

    Infrastructure: Download releases at build time

--- service/scripts/scala-build-5.sh
+++ service/scripts/scala-build-5.sh
@@ -17,9 +17,18 @@ export JAVACMD=$JAVA6
 rm -rf out/lib/5.0.0
 mkdir -p out/lib/5.0.0
 
+if [[ ! -d releases/5.0.0 ]]; then
+  mkdir -p releases/5.0.0
+
+  curl -L https://ccl-artifacts.s3.us-east-1.amazonaws.com/autoconverter-bundles/netlogo-5.0.0.zip -o /tmp/netlogo-5.0.0.zip
+  unzip /tmp/netlogo-5.0.0.zip -d releases/5.0.0
+
+  rm -rf /tmp/netlogo-5.0.0.zip
+fi
+
+cp -r releases/5.0.0/* out/lib/5.0.0
+
 OPTS="-unchecked -deprecation -Xfatal-warnings -Xlint"
 CP="releases/5.0.0/NetLogo.jar:releases/5.0.0/scala-library.jar"
 
 $SCALA2 $OPTS -cp $CP -d out/lib/5.0.0/AutoConverter.jar src/scala/5.0.0/AutoConverter.scala
-
-cp -r releases/5.0.0/* out/lib/5.0.0
--- service/scripts/scala-build-7.sh
+++ service/scripts/scala-build-7.sh
@@ -17,9 +17,18 @@ export JAVACMD=$JAVA17
 rm -rf out/lib/7.0.4
 mkdir -p out/lib/7.0.4
 
+if [[ ! -d releases/7.0.4 ]]; then
+  mkdir -p releases/7.0.4
+
+  curl -L https://ccl-artifacts.s3.us-east-1.amazonaws.com/autoconverter-bundles/netlogo-7.0.4.zip -o /tmp/netlogo-7.0.4.zip
+  unzip /tmp/netlogo-7.0.4.zip -d releases/7.0.4
+
+  rm -rf /tmp/netlogo-7.0.4.zip
+fi
+
+cp -r releases/7.0.4/* out/lib/7.0.4
+
 OPTS="-unchecked -deprecation -Werror -Wunused:linted"
 CP="releases/7.0.4/*.jar"
 
 $SCALA3 $OPTS -cp $CP -d out/lib/7.0.4/AutoConverter.jar src/scala/7.0.4/AutoConverter.scala
-
-cp -r releases/7.0.4/* out/lib/7.0.4
