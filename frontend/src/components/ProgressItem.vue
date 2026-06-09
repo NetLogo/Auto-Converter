30bf8043dd04a66423be4e85529a16974305fdab
Omar Ibrahim <omaribrahim@uchicago.edu>
2026-06-09 15:09:21 -0500

    Lint: Prefer idiomatic component naming cases

--- frontend/src/components/ProgressItem.vue
+++ frontend/src/components/ProgressItem.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
   import { type ConversionStatus } from "@/store/status.js";
 
-  import succeeded from "./Succeeded.vue";
-  import failed from "./Failed.vue";
-  import converting from "./Converting.vue";
+  import Succeeded from "./Succeeded.vue";
+  import Failed from "./Failed.vue";
+  import Converting from "./Converting.vue";
 
   const props = defineProps<{
     item: ConversionStatus
@@ -13,8 +13,8 @@
 <template>
   <div class="round-container progress-item loose-space" :class="props.item.status">
     <p class="progress-item-name">{{ item.name }}</p>
-    <converting v-if="item.status == 'converting'" />
-    <succeeded v-else-if="item.status == 'succeeded'" :file="item.file" :data="item.data" />
-    <failed v-else />
+    <Converting v-if="item.status == 'converting'" />
+    <Succeeded v-else-if="item.status == 'succeeded'" :file="item.file" :data="item.data" />
+    <Failed v-else />
   </div>
 </template>
--- frontend/src/components/ProgressList.vue
+++ frontend/src/components/ProgressList.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
   import { store } from "@/store/store.js";
 
-  import progressItem from "./ProgressItem.vue";
+  import ProgressItem from "./ProgressItem.vue";
 </script>
 
 <template>
   <div id="progress-list" class="flex-column">
-    <progressItem v-for="item of store.getConversions()" :key="item.id" :item="item" />
+    <ProgressItem v-for="item of store.getConversions()" :key="item.id" :item="item" />
   </div>
 </template>
