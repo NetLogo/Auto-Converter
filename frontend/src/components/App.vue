2bb05ba3e8f1f16d462193c1c6de929ce05df185
Omar Ibrahim <omaribrahim@uchicago.edu>
2026-06-09 15:09:21 -0500

    Lint: Prefer idiomatic component naming cases

--- frontend/src/components/App.vue
+++ frontend/src/components/App.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
   import { store } from "@/store/store.js";
 
-  import progressList from "./ProgressList.vue";
-  import resetButton from "./Reset.vue";
-  import selectButton from "./Select.vue";
+  import ProgressList from "./ProgressList.vue";
+  import ResetButton from "./Reset.vue";
+  import SelectButton from "./Select.vue";
 </script>
 
 <template>
   <div id="container" class="flex-column">
     <h1>NetLogo Auto-Converter</h1>
-    <progressList v-if="store.inProgress()" />
-    <resetButton v-if="store.inProgress()" />
-    <selectButton v-else />
+    <ProgressList v-if="store.inProgress()" />
+    <ResetButton v-if="store.inProgress()" />
+    <SelectButton v-else />
   </div>
 </template>
