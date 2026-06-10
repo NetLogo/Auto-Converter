56edc262d64d996853c000c1556103e6483c3b2b
Omar Ibrahim <omaribrahim@uchicago.edu>
2026-06-09 20:49:12 -0500

    Infrastructure: add vue-router, tailwindcss, and @nuxt/ui (vue)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,15 +3,26 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import svgLoader from 'vite-svg-loader';
+import ui from '@nuxt/ui/vite';
+import vueRouter from 'vue-router/vite';
 
 export default defineConfig({
   plugins: [
+    vueRouter({
+      dts: 'src/route-map.d.ts',
+    }),
     vue(),
+    ui({
+      colorMode: false,
+    }),
     vueDevTools(),
+    svgLoader(),
   ],
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
