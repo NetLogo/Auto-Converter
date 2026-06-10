59e1a1b12e9da8337765e6a08c393a8d40957a68
Omar Ibrahim <omaribrahim@uchicago.edu>
2026-06-09 21:31:05 -0500

    Enhancement: Utilize vue-router for pages, add /about page

--- frontend/src/components/App.vue
+++ frontend/src/components/App.vue
@@ -1,22 +1,13 @@
 <script setup lang="ts">
 import AppHeader from '@/components/layout/AppHeader.vue';
-import Headline from '@/components/layout/Headline.vue';
-import FileUploader from '@/components/conversion/FileUploader.vue';
-import ConversionPanel from '@/components/conversion/ConversionPanel.vue';
-import { useConversions } from '@/composables/useConversions.js';
-
-const { inProgress } = useConversions();
 </script>
 
 <template>
   <UApp>
     <AppHeader />
 
-    <UMain id="container" class="flex flex-col items-center justify-center gap-6">
-      <Headline />
-
-        <ConversionPanel v-if="inProgress" />
-        <FileUploader v-else />
+    <UMain id="container" class="flex flex-col items-center justify-center">
+      <RouterView />
     </UMain>
   </UApp>
 </template>
