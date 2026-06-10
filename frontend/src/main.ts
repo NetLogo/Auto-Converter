6178ab3099819e7136e874efb71d53da8cbecb64
Omar Ibrahim <omaribrahim@uchicago.edu>
2026-06-09 20:50:06 -0500

    Infrastructure: wire up vue-router and @nuxt/ui to main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,17 @@
-import "./main.css";
+import './main.css';
 
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 import App from './components/App.vue';
+import ui from '@nuxt/ui/vue-plugin';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [],
+});
+
+app.use(router);
+app.use(ui);
+
+app.mount('#app');
