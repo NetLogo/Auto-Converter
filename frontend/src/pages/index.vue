29d8ebe008fb5274bad3991dbafd5ec4c9b09c40
ERSUCC <isaacbrown5763@gmail.com>
2026-06-11 01:07:24 +0000

    Bugfix: Remove mention of code highlighting from home page

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -26,8 +26,7 @@ useSeo({
         <p class="-mt-6 flex items-center gap-1.5 text-xs md:text-sm text-center text-muted">
           <UIcon name="mdi:information-outline" class="size-4 shrink-0" />
           <span>
-            New here? See how conversion works<span class="hidden md:inline">
-              and how code changes are highlighted</span>.
+            New here? See how conversion works.
             <UButton to="/about" variant="link" color="primary" label="Learn more" class="text-xs md:text-sm px-0.5" />
           </span>
         </p>
